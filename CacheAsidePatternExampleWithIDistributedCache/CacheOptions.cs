b1a59e22d6b861ac4c0e3ec83b32edf7e5e6ed89
rei <67372053+reiugit@users.noreply.github.com>
2024-07-20 14:23:48 +0200

    Implemented field backed Property.

--- CacheAsidePatternExampleWithIDistributedCache/CacheOptions.cs
+++ CacheAsidePatternExampleWithIDistributedCache/CacheOptions.cs
@@ -4,8 +4,11 @@ namespace CacheAsidePatternExampleWithIDistributedCache;
 
 public static class CacheOptions //for brevity without options pattern and without DI
 {
-    public static DistributedCacheEntryOptions AbsoluteExpirationInFiveSeconds = new()
+    private static readonly DistributedCacheEntryOptions absoluteExpirationInFiveSeconds = new()
     {
         AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5)
     };
+
+    public static DistributedCacheEntryOptions AbsoluteExpirationInFiveSeconds
+        => absoluteExpirationInFiveSeconds;
 }
